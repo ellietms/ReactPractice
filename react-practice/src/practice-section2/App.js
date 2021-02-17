3cb2628044c9ac3b3e6142c144d2dfa6f1924afa
ellie <ellie.tms1371@gmail.com>
2021-02-17 23:06:42 +0000

    initializing

---
+++ react-practice/src/practice-section2/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class App extends React.Component{
+  render()
+{    return(
+     <p> Helloo Ellie </p>
+    )
+ }
+} 
+
+
+ReactDOM.render(<App /> , document.getElementById("root"));
